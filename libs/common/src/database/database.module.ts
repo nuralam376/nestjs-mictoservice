4c526e176455ca65176499a54081dce96df2670c
Nur A Alam <nuraalam376@gmail.com>
2025-09-11 18:57:24 +0600

    abstract repository

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import {  ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '../config/config.module';
 
